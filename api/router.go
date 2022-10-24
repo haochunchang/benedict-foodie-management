eaa110ee35779aad4f47d9c50e91b9112a4ecb13
Hao Chun Chang <changhaochun84@gmail.com>
2022-10-24 23:54:13 +0800

    Add swagger API documentation generation

--- api/router.go
+++ api/router.go
@@ -3,10 +3,24 @@ package main
 import (
 	"foodie_manager/controller"
 	"foodie_manager/db"
+	docs "foodie_manager/docs"
 
 	"github.com/gin-gonic/gin"
+	swaggerfiles "github.com/swaggo/files"     // swagger embed files
+	ginSwagger "github.com/swaggo/gin-swagger" // gin-swagger middleware
 )
 
+// @BasePath /api/v1
+
+// PingExample godoc
+// @Summary ping example
+// @Schemes
+// @Description do ping
+// @Tags example
+// @Accept json
+// @Produce json
+// @Success 200 {string} Helloworld
+// @Router /example/helloworld [get]
 func HelloWorld(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"message": "This is the backend of Benedict Foodie Management",
@@ -15,7 +29,9 @@ func HelloWorld(c *gin.Context) {
 
 func setupRouter(repos map[string]interface{}) *gin.Engine {
 	r := gin.Default()
+	docs.SwaggerInfo.BasePath = ""
 	r.GET("/", HelloWorld)
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	food, ok := repos["food"].(*db.FoodRepositoryPSQL)
 	if !ok {
