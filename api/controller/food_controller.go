dc2d3b3f8e7e73bea6fd132cda6685759c1dea63
Hao Chun Chang <changhaochun84@gmail.com>
2022-10-24 23:40:02 +0800

    Add UpdateFoodByName controller

--- api/controller/food_controller.go
+++ api/controller/food_controller.go
@@ -2,6 +2,7 @@ package controller
 
 import (
 	"encoding/json"
+	"fmt"
 	"foodie_manager/db"
 	"io/ioutil"
 
@@ -46,8 +47,34 @@ func GetFoodByName(repo db.FoodRepository) gin.HandlerFunc {
 	}
 }
 
+func UpdateFoodByName(repo db.FoodRepository) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		name := c.Query("oldFoodName")
+
+		var data []byte
+		var err error
+		var food db.Food
+		if data, err = ioutil.ReadAll(c.Request.Body); err != nil {
+			c.JSON(400, gin.H{"message": "Error when reading request body."})
+			return
+		}
+		if err = json.Unmarshal(data, &food); err != nil {
+			c.JSON(400, gin.H{"message": "Error when parsing request body."})
+			return
+		}
+		if err = repo.UpdateFoodByName(name, food); err != nil {
+			c.JSON(500, gin.H{"message": "Something error when updating food."})
+			return
+		}
+		c.JSON(200, gin.H{
+			"message": fmt.Sprintf("Food %s updated to %s", name, food.Name),
+		})
+	}
+}
+
 func SetupFoodControllers(r *gin.Engine, repo db.FoodRepository) *gin.Engine {
 	r.GET("/foods/:name", GetFoodByName(repo))
 	r.POST("/foods", CreateFood(repo))
+	r.PUT("/foods", UpdateFoodByName(repo))
 	return r
 }
--- api/controller/food_controller_test.go
+++ api/controller/food_controller_test.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"foodie_manager/db"
 	"net/http"
 	"net/http/httptest"
@@ -77,4 +78,56 @@ func TestGetFoodbyNameRoute(t *testing.T) {
 	if resp.Description != food.Description {
 		t.Errorf("Food Description not correct\nExpected %s, got %s", food.Description, resp.Description)
 	}
+}
+
+func TestUpdateFoodByNameRoute(t *testing.T) {
+	router := SetupFoodControllers(gin.Default(), foodRepo)
+
+	// POST sample food
+	food := getSampleFood()
+	json_data, _ := json.Marshal(food)
+	req, _ := http.NewRequest("POST", "/foods", bytes.NewBuffer(json_data))
+	router.ServeHTTP(httptest.NewRecorder(), req)
+
+	// PUT should respond success
+	food.Name = "hihi"
+	json_data, _ = json.Marshal(food)
+
+	w := httptest.NewRecorder()
+	req, _ = http.NewRequest("PUT", "/foods?oldFoodName=hororo", bytes.NewBuffer(json_data))
+	router.ServeHTTP(w, req)
+	if w.Code != 200 {
+		t.Errorf("Status code not 200, got %d", w.Code)
+	}
+	if w.Body.String() != fmt.Sprintf(`{"message":"Food %s updated to %s"}`, "hororo", "hihi") {
+		t.Errorf("Food name not updated, got %s", w.Body.String())
+	}
+
+	// w = httptest.NewRecorder()
+	// req, _ = http.NewRequest("GET", "/foods/hororo", nil)
+	// router.ServeHTTP(w, req)
+	// if w.Code != 200 || w.Body.String() != `"message": "Food not found."` {
+	// 	t.Errorf("Get by old name should not be found, got %s", w.Body.String())
+	// }
+
+	// Get new name should get data
+	w = httptest.NewRecorder()
+	req, _ = http.NewRequest("GET", "/foods/hihi", nil)
+	router.ServeHTTP(w, req)
+	if w.Code != 200 {
+		t.Errorf("Status code not 200, got %d", w.Code)
+	}
+
+	var resp db.Food
+	if err := json.NewDecoder(w.Body).Decode(&resp); err != nil {
+		t.Fatalf("Decode response error: %v", err)
+	}
+	fmt.Print(resp)
+	if resp.Name != "hihi" {
+		t.Errorf("Food name not correct\nExpected hihi, got %s", resp.Name)
+	}
+}
+
+func TestDeleteFoodRoute(t *testing.T) {
+
 }
